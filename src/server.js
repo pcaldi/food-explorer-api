75a9d13dd47728cf6a8165427f3542af26917100
pcaldi <pcaldi@gmail.com>
2024-08-15 18:24:50 -0300

    feat: adicionando cors na aplicação

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+})
 
 const PORT = 3333;
 app.listen(PORT, () => {
