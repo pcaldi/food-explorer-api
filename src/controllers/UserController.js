9cd3064e496465bb33631898e940bf4b83accdce
pcaldi <pcaldi@gmail.com>
2024-08-19 18:42:53 -0300

    feat: adicionando sqlite

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,7 +1,14 @@
 
+const AppError = require('../utils/AppError')
+
 class UserController {
   create(req, res) {
     const { name, email, password } = req.body;
+
+
+    if (!name || !email || !password) {
+      throw new AppError("Preencha todos os campos", 400);
+    }
 
     res.json({ name, email, password });
   }
