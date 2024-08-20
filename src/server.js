971d75fd8a577545df2def1f5e5cceaf14c57838
pcaldi <pcaldi@gmail.com>
2024-08-20 20:21:04 -0300

    alterando o caminho database

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const AppError = require('./utils/AppError');
 
-const database = require('../src/database/sqlite');
+const database = require('./database/sqlite');
 
 const routes = require('./routes');
 
