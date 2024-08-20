969b64748b0bfbd468a961e753ada858a165dd81
pcaldi <pcaldi@gmail.com>
2024-08-20 08:54:44 -0300

    feat: cadastrando usuário

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,16 +1,34 @@
+const { hash } = require('bcryptjs');
 
-const AppError = require('../utils/AppError')
+const AppError = require('../utils/AppError');
+
+const sqlConnection = require('../database/sqlite');
 
 class UserController {
-  create(req, res) {
-    const { name, email, password } = req.body;
+  async create(request, response) {
+    const { name, email, password } = request.body;
 
+    const database = await sqlConnection()
 
-    if (!name || !email || !password) {
-      throw new AppError("Preencha todos os campos", 400);
+    const checkUserExists = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (checkUserExists) {
+      throw new AppError('E-mail já está em uso.');
     }
 
-    res.json({ name, email, password });
+    const HASH_SALT = 8;
+
+    const hashedPassword = await hash(password, HASH_SALT);
+
+    await database.run(
+      "INSERT INTO users (name,email, password) VALUES (?, ?, ?)",
+      [name, email, hashedPassword]
+    );
+
+    return response.status(201).json({});
   }
 
 }
