23ddfe447e08d532c06162669175fc8a06d54334
pcaldi <pcaldi@gmail.com>
2024-09-03 12:40:29 -0300

    criando sessions controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require('express');
 
 const userRoutes = require('./users.routes');
 const dishesRoutes = require('./dishes.routes');
+const sessionsRoutes = require('./sessions.routes');
 
 const routes = Router();
 
 routes.use('/users', userRoutes);
 routes.use('/dishes', dishesRoutes);
+routes.use('/sessions', sessionsRoutes);
 
 module.exports = routes;
