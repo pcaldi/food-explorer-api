6b384c8b4a8b8c26d4e9647785fdc159350caff8
pcaldi <pcaldi@gmail.com>
2024-09-05 15:36:22 -0300

    adicionando coluna  is_admin

--- src/database/knex/migrations/20240820133207_createUsers.js
+++ src/database/knex/migrations/20240820133207_createUsers.js
@@ -3,7 +3,7 @@ exports.up = knex => knex.schema.createTable("users", table => {
   table.text("name");
   table.text("email");
   table.text("password");
-  table.text("avatar").nullable();
+  table.text("is_admin").defaultTo(false);
 
   table.timestamp("created_at").defaultTo(knex.fn.now());
   table.timestamp("updated_at").defaultTo(knex.fn.now());
