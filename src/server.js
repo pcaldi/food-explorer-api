dde89984df582922742e581ceb32d3d84528c8c1
pcaldi <pcaldi@gmail.com>
2024-10-22 18:59:39 -0300

    instalando dotenv

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require('express-async-errors');
+require('dotenv/config');
 
 const express = require('express');
 const cors = require('cors');
@@ -35,7 +36,7 @@ app.use((error, request, response, next) => {
 
 })
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
   console.log(`Server is running on port 🚀 ${PORT}`);
 })
